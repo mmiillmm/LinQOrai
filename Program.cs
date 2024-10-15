194a6b48701c60efa13989939ec0bba4de5ccb70
mmiillmm <kmakai520@gmail.com>
2024-10-15 13:07:32 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Unicode;
+using Verseny;
+
+List<Versenyzo> versenyzok = [];
+
+using StreamReader reader = new("forras.txt");
+while (!reader.EndOfStream) versenyzok.Add(new(reader.ReadLine()));
+
+Console.WriteLine($"Versenyzők száma: {versenyzok.Count}");
+
+//első célba érkező férfi
+
---
+++ Versenyzo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Verseny;
+
+internal class Versenyzo
+{
+    public string Nev { get; set; }
+    public int SzulEv { get; set; }
+    public string RajtSzam { get; set; }
+    public bool Nem { get; set; }
+    public string Kategoria { get; set; }
+    public Dictionary<string, TimeSpan> VersenyIdo { get; set; }
+
+    public int TotalTimeInSeconds => /*Casting =>*/(int)VersenyIdo.Values.Sum(ts => ts.TotalSeconds);
+
+    public override string ToString() =>
+        $"[{RajtSzam}] {Nev} {(Nem ? "Ferfi" : "Nő")} {Kategoria}";
+
+    public Versenyzo(string sor)
+    {
+        var split = sor.Split(';');
+        Nev = split[0];
+        SzulEv = int.Parse(split[1]);
+        RajtSzam = split[2];
+        Nem = split[3] == "f";
+        Kategoria = split[4];
+        VersenyIdo = new()
+        {
+            {"swimming", TimeSpan.Parse(split[5])},
+            {"1st depot", TimeSpan.Parse(split[6])},
+            {"cycling", TimeSpan.Parse(split[7])},
+            {"2nd depot", TimeSpan.Parse(split[8])},
+            {"running", TimeSpan.Parse(split[9])},
+        };
+    }
+
+}
